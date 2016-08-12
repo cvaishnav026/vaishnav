020af751ebaa8582e5130dcaf20c07fdf566221d
cvaishnav026 <vaishvaishnav0@gmail.com>
2016-08-12 22:18:17 +0530

    security implemented in project 2

---
+++ LetsTalk4(security)/src/main/java/com/niit/services/UserServiceImple.java
@@ -0,0 +1,44 @@
+package com.niit.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.niit.dao.UserDAO;
+import com.niit.model.User;
+
+@Service
+@Transactional
+public class UserServiceImple implements UserService{
+	
+	@Autowired
+	private UserDAO userdao;
+
+	@Override
+	public void saveOrUpdate(User user) {
+		System.out.println("in saveorupdate services");
+		userdao.saveOrUpdate(user);
+		
+	}
+
+	@Override
+	public List<User> list() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User getUserByName(String username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User getUserId(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
